decb809d3433c79be046c147c20cb6531438c8ba
KailashV <kailashventhan123@gmail.com>
2026-06-29 12:29:44 -0700

    setting up int to roman solution class

---
+++ int_roman.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+
+class Solution {
+public:
+   std::string intToRoman(int num){
+      
+      std::cout << "X" << std::endl;
+      return "X";
+   }
+
+   Solution(){
+
+   }
+
+   ~Solution(){
+    
+   }
+};
+
+
+int main(){
+   Solution solution;
+   solution.intToRoman(100); 
+}
