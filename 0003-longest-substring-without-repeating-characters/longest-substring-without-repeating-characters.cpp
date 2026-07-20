260cbb91317981c1171becfc41267ed1047e4e95
Kailash Venthan <54091963+kailashv7@users.noreply.github.com>
2026-07-19 18:36:46 -0700

    Longest Substring Without Repeating Characters — C++ (Runtime 8 ms, Memory 11.7 MB)

--- 0003-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char, int> chars;
-        int left = 0;
-        int right = 0;
-        int res = 0;
-        while (right < s.length()) {
-            char r = s[right];
-            chars[r]++;
-            while (chars[r] > 1) {
-                char l = s[left];
-                chars[l]--;
-                left++;
-            }
-            res = max(res, right - left + 1);
-            right++;
-        }
-        return res;
+        unordered_map<char,int> chars;
+int left = 0, right = 0, res = 0;
+while (right < s.length()) {
+  chars[s[right]]++;            // expand
+  while (chars[s[right]] > 1) { // invalid?
+    chars[s[left]]--; left++;   // shrink
+  }
+  res = max(res, right - left + 1);
+  right++;
+}
+return res;
     }
 };
