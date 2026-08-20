4c3e9763c4cbca7e3b4056c8e5fd64621e5463a5
Kailash Venthan <54091963+kailashv7@users.noreply.github.com>
2026-08-19 19:56:42 -0700

    Number of Islands — C++ (Runtime 29 ms, Memory 16.3 MB)

--- 0200-number-of-islands/number-of-islands.cpp
+++ 0200-number-of-islands/number-of-islands.cpp
@@ -1,23 +1,19 @@
 class Solution {
+    int rows, cols;
+    void sink(vector<vector<char>>& grid, int r, int c) {
+        if (r < 0 || r >= rows || c < 0 || c >= cols) return;
+        if (grid[r][c] != '1') return;
+        grid[r][c] = '0';
+        sink(grid, r+1, c); sink(grid, r-1, c);
+        sink(grid, r, c+1); sink(grid, r, c-1);
+    }
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int rows = grid.size(), cols = grid[0].size();
+        rows = grid.size(); cols = grid[0].size();
         int count = 0;
-
-        function<void(int,int)> sink = [&](int r, int c) {
-            if (r < 0 || r >= rows || c < 0 || c >= cols) return;
-            if (grid[r][c] != '1') return;
-            grid[r][c] = '0';
-            sink(r+1, c);
-            sink(r-1, c);
-            sink(r, c+1);
-            sink(r, c-1);
-        };
-
         for (int r = 0; r < rows; ++r)
             for (int c = 0; c < cols; ++c)
-                if (grid[r][c] == '1') { ++count; sink(r, c); }
-
+                if (grid[r][c] == '1') { ++count; sink(grid, r, c); }
         return count;
     }
 };
